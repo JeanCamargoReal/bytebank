560f7c215bd67817d34196151648077e4cbb6631
JeanCamargoReal <jeancamargoreal@icloud.com>
2021-11-01 17:05:59 -0300

    Implementing transfer Widget

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/jeancamargo/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jeancamargo/Developer/Alura/Flutter/bytebank"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,26 @@
 
 import 'package:flutter/material.dart';
 
-void main() {
+void main() { 
   runApp(MaterialApp(
     home: Scaffold(
+      body: Column(
+        children: const [
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('100.0'),
+              subtitle: Text('1000.0')
+            )
+          ),Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('200.0'),
+              subtitle: Text('1000.0')
+            )
+          ),
+        ],
+      ),
       appBar: AppBar(
         title: Text('Tranferências'),
       ),
