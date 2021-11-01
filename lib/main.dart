7ce70edfc6ba1edd2f4d8dd20064f8f9dfe862b0
JeanCamargoReal <jeancamargoreal@icloud.com>
2021-11-01 15:05:57 -0300

    Started project

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Column(
+      children: <Widget>[
+        Text(
+          'Deliver features faster',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Column(
+          children: <Widget>[
+            Text(
+              'Craft beautiful UIs',
+              textDirection: TextDirection.ltr,
+            ),
+          ],
+        )
+      ],
+    ),
+  );
+}
